09f4f3e1e3e69adca10e3a354fcece99d30c1b5a
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2023-08-29 13:03:43 +0100

    chore: update requirements.txt

--- main.py
+++ main.py
@@ -40,6 +40,26 @@ def get_db():
     finally:
         db.close()
 
+# fetch updated data
+async def fetch_updated_data(last_sync_timestamp, cursor=None, page_size=None):
+    modified_employees = client.hris.employees.list(modified_after=last_sync_timestamp,cursor=cursor, page_size=page_size)
+    return modified_employees
+
+# sync every 24 hours
+@app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 24)
+def sync():
+    fetch_updated_data()
+
+# respond to webhook requests
+@app.post('/employee-modifications')
+async def root(json_data: Dict[str, Any]):
+    return json_data
+
+@app.get('/employee-modifications')
+async def get_employee_modifications(last_sync_timestamp, cursor, page_size):
+    updated_data = await fetch_updated_data(last_sync_timestamp, cursor, page_size)
+    return updated_data
 
 
 @app.get("/tasks/", response_model=List[schemas.Task])
